5649cf53ca8f47efd485e9fa7984f1807aea94d2
artmayer3 <a.seliverstovqq@gmail.com>
2021-01-06 00:28:03 +0500

    Create MMF.cs

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Terminal
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.ReadLine();
+            MMF.CheckMMF();
         }
     }
 }
