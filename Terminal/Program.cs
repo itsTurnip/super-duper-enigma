6a746822c3a1802ec05bf07a52a17e0e702fa7b2
artmayer3 <a.seliverstovqq@gmail.com>
2021-01-11 05:33:29 +0500

    Change Main

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -3,14 +3,15 @@ using ConsoleLib;
 
 namespace Terminal
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             ConsoleMan man = new ConsoleMan();
+            MMF mmf = new MMF();
             foreach (string el in "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat ut metus eu feugiat. Suspendisse non tellus in risus posuere dapibus. Suspendisse volutpat in quam non lobortis. Aliquam elementum purus erat, vitae viverra ex semper a. Phasellus vel ullamcorper odio. Donec molestie est non pharetra gravida. Cras iaculis, mauris quis iaculis volutpat, turpis dui sagittis neque, sed hendrerit libero arcu non mauris. Nullam at consectetur tortor. Vivamus dictum dapibus ex, nec posuere orci. Sed consectetur fermentum mollis. Suspendisse commodo nisl eu leo egestas, rhoncus finibus magna sagittis.".Split())
                 man.WindowList.Add(el);
-            MMF.CheckMMF();
+            mmf.CheckMMF();
             man.ShowMessage("Bullshit happens");
             man.Loop();
         }
