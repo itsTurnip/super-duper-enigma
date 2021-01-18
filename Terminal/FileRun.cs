6e47a720d7458ab38a16ec3da94d4e2dc82b4c56
artmayer3 <a.seliverstovqq@gmail.com>
2021-01-18 19:46:51 +0500

    Fix some shit

--- Terminal/FileRun.cs
+++ Terminal/FileRun.cs
@@ -4,22 +4,33 @@ namespace Terminal
 {
     internal class FileRun
     {
-        public static void ExecuteCommand()
+        public static void Run()
         {
-            var processInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/c " + " regsvr32" + @" WSCRun.wsc")
+            Type procT = Type.GetTypeFromProgID("WSCRun.WSC");
+
+            if (procT == null)
             {
-                CreateNoWindow = false,
-                Verb = "runas",
-                UseShellExecute = true
-            };
-            using (var process = System.Diagnostics.Process.Start(processInfo))
-            {
-                process.WaitForExit();
+                var processInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe", "/c regsvr32 WSCRun.wsc")
+                {
+                    CreateNoWindow = false,
+                    Verb = "runas",
+                    UseShellExecute = true
+                };
+                using (var process = System.Diagnostics.Process.Start(processInfo))
+                {
+                    process.WaitForExit();
+                }
+                procT = Type.GetTypeFromProgID("WSCRun.WSC");
             }
 
-            Type procT = Type.GetTypeFromProgID("WSCRun.WSC");
-            dynamic proc = Activator.CreateInstance(procT);
-            proc.Run();
+            try
+            {
+                dynamic proc = Activator.CreateInstance(procT);
+                proc.Run();
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
